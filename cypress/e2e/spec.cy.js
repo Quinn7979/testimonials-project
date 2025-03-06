03f1200adcdf6bdf7d3ca02f6ccab046c8ce9ff9
crystal <crysbonnie@gmail.com>
2025-03-06 13:33:27 -0700

    u

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe("testimonials", () => {
 	it("list testimonials", () => {
-		cy.visit("localhost:3000/testimonials-project");
+		cy.visit("localhost/testimonials-project");
 
 		cy.get("h1").should("have.text", "testimonials").should("be.visible");
 		cy.get("ul[name=testimonials-list").should("be.visible");
